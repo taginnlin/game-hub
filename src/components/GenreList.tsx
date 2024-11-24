e61ee913535fcf8baa20f63024f3df2e5ead6a85
taginnlin <2420891901@qq.com>
2024-11-24 16:50:44 +0800

    Add margin above genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -23,7 +23,7 @@ const GenreList = ({ onSelectedGenre, selectedGenre }: Props) => {
 
   return (
     <>
-      <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
+      <Heading fontSize='2xl' marginTop={9} marginBottom={3}>Genres</Heading>
       <List>
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
